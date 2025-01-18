f8365db11c15e555ac57701b002457003977a2af
hongsy521 <seoyeong0521@naver.com>
2025-01-18 11:56:35 +0900

    feat : CustomWithMockUser 생성 후 SecurityContext에 등록

---
+++ src/test/java/com/sparta/onboarding/mock/WithCustomMockUser.java
@@ -0,0 +1,11 @@
+package com.sparta.onboarding.mock;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import org.springframework.security.test.context.support.WithSecurityContext;
+
+@Retention(RetentionPolicy.RUNTIME)
+@WithSecurityContext(factory = WithCustomMockUserSecurityContextFactory.class)
+public @interface WithCustomMockUser {
+    String username() default "HONG";
+}
