6d3fa82d09e8112a64cfc5c49939499b73836b52
hongsy521 <seoyeong0521@naver.com>
2025-01-18 16:26:32 +0900

    feat : repository test code

---
+++ src/test/java/com/sparta/onboarding/auth/UserRepositoryTest.java
@@ -0,0 +1,47 @@
+package com.sparta.onboarding.auth;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import com.sparta.onboarding.auth.model.User;
+import java.util.Optional;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+@DataJpaTest
+class UserRepositoryTest {
+    @Autowired
+    private UserRepository userRepository;
+
+    @Test
+    void testFindByUsername() {
+        // given
+        String username = "testUser";
+        User user = User.builder()
+            .username(username)
+            .password("1234")
+            .nickname("닉네임")
+            .build();
+
+        userRepository.save(user);
+
+        // when
+        Optional<User> findUser = userRepository.findByUsername(username);
+
+        // then
+        assertTrue(findUser.isPresent());
+        assertEquals(username, findUser.get().getUsername());
+    }
+
+    @Test
+    void testFindByUsername_NotFound() {
+        // given
+        String username = "nonExistingUser";
+
+        // when
+        Optional<User> findUser = userRepository.findByUsername(username);
+
+        // then
+        assertFalse(findUser.isPresent());
+    }
+}
